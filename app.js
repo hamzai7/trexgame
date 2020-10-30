822477c68fc227176a9b9f611f7182a8412a1e7d
hamzai7 <hamzaiqbal.hi7@gmail.com>
2020-10-29 21:34:20 -0500

    added gravity to jumps and function for obstaceles

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 const dino = document.querySelector('.dino')
 let isJumping = false
-let gravity 
+let gravity = 0.9
 
 function control(e) {
     if (e.keyCode === 32) {
@@ -13,32 +13,41 @@ function control(e) {
 }
 document.addEventListener('keyup', control)
 
+let position = 0
 function jump() {
-    let position = 0
+    let count = 0
     let timerId = setInterval(function (){
-
         // move down
-        if (position === 150) {
+        if (count === 15) {
             clearInterval(timerId)
             console.log('down')
             let downTimerId = setInterval(function () {
-                if (position ===0) {
+                if (count === 0) {
                     clearInterval(downTimerId)
                     isJumping = false
                 }
-                position -= 30
+                position -= 5
+                count--
+                position = position * gravity
                 dino.style.bottom = position + 'px'
             },20)
             
         }
-
-
-
         //move up
         console.log('up')
         position +=30
+        count++
+        position = position * gravity
         dino.style.bottom = position + 'px'
+        console.log(dino.style.bottom)
     },20)
 }
+
+function generateObstacles () {
+    let obstaclePosition = 1000
+    const obstacle = document.createElement('div')
+    obstacle.classList.add('obstacle')
+}
+
 
 })
